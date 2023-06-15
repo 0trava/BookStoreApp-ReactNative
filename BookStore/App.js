cb88c4f2be18678ec7ef6de8350ee784d328c8fc
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-15 21:10:57 +0300

    fix bug with versions

--- BookStore/App.js
+++ BookStore/App.js
@@ -1,50 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
-import { BookDetail } from './screens';
-import Tabs from './navigation/tabs';
+import { BookDetail } from "./screens/";
+import Tabs from "./navigation/tabs";
 
 const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    border: "transparent"
-  }
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent"
+    }
 }
 
 const Stack = createStackNavigator();
 
+const App = () => {
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                {/* Tabs */}
+                <Stack.Screen name="Home" component={Tabs} />
 
-export default function App() {
-  return (
-    <NavigationContainer theme={theme}>
-      <Stack.Navigator
-          screenOptions={{
-            headerShown: false
-          }}
-          initialRouteName={''}
-      >
-        {/* TABS */}
-        <Stack.Screen name="Home" component={Tabs} />
-
-        {/* Screen */}
-        <Stack.Screen name="BookDetail" component={BookDetail} options={{headerShown: false}}/>
-
-      </Stack.Navigator>
-    </NavigationContainer>
-
-  );
+                {/* Screens */}
+                <Stack.Screen name="BookDetail" component={BookDetail} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+export default App;
--- BookStore/navigation/tabs.js
+++ BookStore/navigation/tabs.js
@@ -1,83 +1,84 @@
 import React from "react";
-import {Image} from "react-native";
-import  {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {Home} from '../screens/Home' ;
-import {COLORS, icons} from '../constants/' ;
+import {
+    Image
+} from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home } from "../screens/";
+import { icons, COLORS } from "../constants";
 
-const Tab=createBottomTabNavigator();
-
+const Tab = createBottomTabNavigator();
 
 const tabOptions = {
     showLabel: false,
     style: {
         height: "10%",
-        backgroundColor: COLORS.black,
+        backgroundColor: COLORS.black
     }
-
 }
 
-
-// NAVIGATION
 const Tabs = () => {
     return (
         <Tab.Navigator
-        tabOptions={tabOptions}
-        screenOptions={({route}) => ({
-            tabBarIcon: ({focused}) => {
-                const tintColor = focused ? COLORS.white : COLORS.gray;
+            tabBarOptions={tabOptions}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused }) => {
+                    const tintColor = focused ? COLORS.white : COLORS.gray;
 
-                switch(route.name) {
-                    case "Home":
-                        return(
-                            <Image 
-                                source={icons.dashboard_icon}
-                                resizeMode="contain"
-                                style={{
-                                    tintColor: tintColor,
-                                    width: 25,
-                                    height: 25,
-                                }}
-                            />
-                        )
-                    case "Search":
-                        return(
-                            <Image 
-                                 source={icons.search_icon}
-                                resizeMode="contain"
-                                 style={{
-                                      tintColor: tintColor,
-                                      width: 25,
-                                      height: 25,
-                                 }}
-                             />
-                         )
-                    case "Notification":
-                        return(
-                            <Image 
-                                source={icons.notification_icon}
-                                resizeMode="contain"
-                                style={{
-                                    tintColor: tintColor,
-                                    width: 25,
-                                    height: 25,
-                                     }}
-                            />
-                        )
-                    case "Setting":
-                        return(
-                            <Image 
-                                source={icons.menu_icon}
-                                resizeMode="contain"
-                                style={{
-                                    tintColor: tintColor,
-                                    width: 25,
-                                    height: 25,
-                                        }}
-                            />
-                        )
+                    switch (route.name) {
+                        case "Home":
+                            return (
+                                <Image
+                                    source={icons.dashboard_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+
+                        case "Search":
+                            return (
+                                <Image
+                                    source={icons.search_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+
+                        case "Notification":
+                            return (
+                                <Image
+                                    source={icons.notification_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+
+                        case "Setting":
+                            return (
+                                <Image
+                                    source={icons.menu_icon}
+                                    resizeMode="contain"
+                                    style={{
+                                        tintColor: tintColor,
+                                        width: 25,
+                                        height: 25
+                                    }}
+                                />
+                            )
+                    }
                 }
-            }
-        })}
+            })}
         >
             <Tab.Screen
                 name="Home"
@@ -95,11 +96,8 @@ const Tabs = () => {
                 name="Setting"
                 component={Home}
             />
-
-
         </Tab.Navigator>
     )
-
 }
 
-export default Tabs
+export default Tabs;
