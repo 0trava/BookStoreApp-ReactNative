7af24e021c550672428f2c43d27591130de0f3d7
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-15 21:40:37 +0300

    add home page

--- BookStore/screens/Home.js
+++ BookStore/screens/Home.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import { 
+    SafeAreaView,
     StyleSheet, 
     Text, 
+    TouchableOpacity, 
     View } from 'react-native';
 
+import { COLORS, FONTS, SIZES, icons, images } from '../constants';
+
 const Home = () => {
+
+    const profileData ={
+        name: 'Username',
+        point: 200,
+
+    }
+
+    const [profile, setProfile] = React.useState(profileData);
+
+    function renderHeader(profile) {
+        return (
+            <View style={{flex: 1, flexDirection: 'row', paddingHorizontal: SIZES.padding, alignItems: 'center' }}>
+                {/* Greetings */}
+                    <View style={{flex: 1}}>
+                        <View style={{marginRight: SIZES.padding}}>
+                            <Text style={{ ...FONTS.h3, color: COLORS.white}}>Good Morning</Text>
+                            <Text style={{ ...FONTS.h2, color: COLORS.white}}>{profile.name}</Text>
+                        </View>
+                    </View>
+
+                {/* Points */}
+                <TouchableOpacity>
+                    
+                </TouchableOpacity>
+            </View>
+        )
+
+    }
+
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+        <SafeAreaView style={{flex:1, backgroundColor:COLORS.black }}>
+            <View style={{ height: 200 }}>
+                {renderHeader(profile)}
+
+            </View>
+
+        </SafeAreaView>
   )
 }
 
