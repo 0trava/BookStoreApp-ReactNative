b0312177b02b4ef3a5d2cd36d1b1473a3d6df270
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-15 16:27:22 +0300

    add

--- BookStore/navigation/tabs.js
+++ BookStore/navigation/tabs.js
@@ -2,11 +2,51 @@ import React from "react";
 import {Image} from "react-native";
 import  {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {Home} from '../screens/Home' ;
-import {COLORS} from '../constants/theme' ;
+import {COLORS, icons} from '../constants/' ;
 
 const Tab=createBottomTabNavigator();
 
 
 const tabOptions = {
-    
+    showLabel: false,
+    style: {
+        height: "10%",
+        backgroundColor: COLORS.black,
+    }
+
 }
+
+
+// NAVIGATION
+const Tabs = () => {
+    return (
+        <Tab.Navigator
+        tabOptions={tabOptions}
+        screenOptions={({route}) => ({
+            tabBarIcon: ({focused}) => {
+                const tintColor = focused ? COLORS.white : COLORS.gray;
+
+                switch(route.name) {
+                    case "Home":
+                        return(
+                            <Image 
+                                source={icons.dashboard_icon}
+                                resizeMode="contain"
+                                style={{
+                                    tintColor: tintColor,
+                                    width: 25,
+                                    height: 25,
+                                }}
+                            />
+                        )
+                }
+            }
+        })}
+        >
+
+        </Tab.Navigator>
+    )
+
+}
+
+export default Tabs
