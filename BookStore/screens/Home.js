4a31d784e6690c11805353a99b7751f26cb63bda
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-15 22:57:38 +0300

    add Home block

--- BookStore/screens/Home.js
+++ BookStore/screens/Home.js
@@ -9,6 +9,15 @@ import {
 
 import { COLORS, FONTS, SIZES, icons, images } from '../constants';
 
+const LinearDivider = () => {
+    return(
+        <View style={{width: 1, paddingVertical: 18}}>
+            <View style={{flex: 1, borderLeftColor: COLORS.lightGray, borderLeftWidth: 1}}></View>
+
+        </View>
+    )
+}
+
 const Home = () => {
 
     const profileData ={
@@ -91,11 +100,47 @@ const Home = () => {
                         borderRadius: SIZES.radius
                     }}>
                         {/* Claim */}
-
+                        <TouchableOpacity
+                            style={{flex:1}}
+                            onPress={()=> console.log("Claim")}>
+                            
+                            <View style={{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                                <Image 
+                                    source={icons.claim_icon}
+                                    resizeMode='contain'
+                                    style={{
+                                        width: 30,
+                                        height: 30,
+                                    }}
+                                />
+                                <Text style={{marginLeft: SIZES.base, ...FONTS.body3, color: COLORS.white}}>Claim</Text>
+                            </View>
+                        </TouchableOpacity>
 
                         {/* Divider */}
+                        <LinearDivider/>
 
+                        {/* Get Point */}
+                        <TouchableOpacity
+                            style={{ flex: 1}}
+                            onPress={()=> console.log("Get Point")}
+                        >
+                            <View style={{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                                <Image 
+                                source={icons.point_icon} 
+                                resizeMode="contain"
+                                style={{
+                                    width: 30,
+                                    height: 30
+                                }}/>
+                                <Text style={{marginLeft: SIZES.base, ...FONTS.body3, color: COLORS.white}}>Get Point</Text>
+                            </View>
+                        </TouchableOpacity>
 
+                        {/* Divider */}
+                        
+
+                        {/* My Card */}
                     </View>
 
             </View>
@@ -114,6 +159,8 @@ const Home = () => {
         </SafeAreaView>
   )
 }
+
+
 
 export default Home;
 
