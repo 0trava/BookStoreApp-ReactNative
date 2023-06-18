6fa07ef9297a0ab764f07303a2b306e8b64d603a
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-18 22:46:43 +0300

    add filter Home

--- BookStore/screens/Home.js
+++ BookStore/screens/Home.js
@@ -338,9 +338,28 @@ const Home = () => {
     }
 
     function renderCategoryHeader() {
+        const renderItem = ({item}) => {
+            return (
+                <TouchableOpacity style={{flex: 1, marginRight: SIZES.padding}}
+                onPress={() => setSelectedCategory(item.id)}>
+                    {selectedCategory == item.id &&
+                    <Text style={{ color: COLORS.white}}>{item.categoryName}</Text>}
+
+                    {selectedCategory != item.id &&
+                    <Text style={{ color: COLORS.lightGray}}>{item.categoryName}</Text>}
+                </TouchableOpacity>
+            )
+        }
+
         return (
             <View style={{flex:1, paddingLeft: SIZES.padding}}>
                 <FlatList 
+                    data={categories}
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={renderItem}
+                    keyExtractor={item => `${item.id}`}
+                    horizontal
+
                     />
 
             </View>
